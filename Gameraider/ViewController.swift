6aac3dbdd2d7732780da71f962f10936066adc7b
Weiyi Kong <kongwy@users.noreply.github.com>
2020-10-15 15:41:32 +0800

    Initial Commit

---
+++ Gameraider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gameraider
+//
+//  Created by Weiyi Kong on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
